eca96569827cb60dc0669a2e62f6d5fd0d18277e
Victor <victor@turo.com>
2018-09-14 09:49:33 -0700

    feat: add getKey option
    allow user to transform reported keys

--- index.js
+++ index.js
@@ -3,37 +3,45 @@
  */
 const path = require("path");
 
-const defaultLogger = {
-    done: stats => {
-        // Only run this if on Team City
-        if (process.env && !process.env.TEAMCITY_VERSION) {
-            return;
-        }
-        const { assetsByChunkName, assets } = stats.toJson();
-
-        assets.forEach(asset => {
-            const chunkName = asset.chunkNames[0];
-            if (!assetsByChunkName[chunkName]) {
-                return;
-            }
-
-            const { name: assetName, size: assetSize } = asset;
-            const chunkFullName = path.basename(assetName);
-
-            console.log(`##teamcity[buildStatisticValue key='${chunkFullName}' value='${assetSize}']`);
-        });
+const createDefaultLogger = options => ({
+  done: stats => {
+    // Only run this if on Team City
+    if (process.env && !process.env.TEAMCITY_VERSION) {
+      return;
     }
-};
+    const { assetsByChunkName, assets } = stats.toJson();
+
+    assets.forEach(asset => {
+      const chunkName = asset.chunkNames[0];
+      if (!assetsByChunkName[chunkName]) {
+        return;
+      }
+
+      const { name: assetName, size: assetSize } = asset;
+      const chunkFullName = path.basename(assetName);
+
+      console.log(
+        `##teamcity[buildStatisticValue key='${options.getKey(
+          chunkFullName
+        )}' value='${assetSize}']`
+      );
+    });
+  }
+});
 
 class WebpackTeamcityBundleSizePlugin {
-    constructor(callback = {}) {
-        this.callback = Object.assign({}, defaultLogger, callback);
-    }
-    apply(compiler) {
-        Object.keys(this.callback).forEach(key => {
-            compiler.plugin(key, this.callback[key]);
-        });
-    }
+  constructor(options = { getKey: fileName => fileName, callback: {} }) {
+    this.callback = Object.assign(
+      {},
+      createDefaultLogger(options),
+      options.callback
+    );
+  }
+  apply(compiler) {
+    Object.keys(this.callback).forEach(key => {
+      compiler.plugin(key, this.callback[key]);
+    });
+  }
 }
 
 module.exports = WebpackTeamcityBundleSizePlugin;
